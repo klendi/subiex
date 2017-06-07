120eeabe03b7864449fb2bf0cd876bc2e10a4e6f
klendigocci <klendigocci@gmail.com>
2017-06-07 16:59:00 +0200

    Added Lorem Generator C-P

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
+const fs = require('fs')
 const config = require('./config')
 const dotenv = require('dotenv')
 dotenv.load()
@@ -10,7 +11,7 @@ bot.on('ready', () => {
 })
 
 bot.on('message', (message) => {
-		if (message.content.startsWith('!')) 
+		if (message.content.startsWith(config.prefix)) 
 		{
 			try
 			{
@@ -19,18 +20,58 @@ bot.on('message', (message) => {
 					case config.prefix + 'report':
 						message.reply('I am online and working!')
 	    				var time = Date.now()
-	   					message.channel.send('Pew').then(m => m.edit('PING TOOK : **' + (Date.now() - time) + '** ms**'))
-	    				message.channel.send('This report was sent in :```' + new Date() + '``` !');
+	   					message.channel.send('Pew').then(m => m.edit('PING TOOK : **' + (Date.now() - time) + '** ms'))
+	    				message.channel.send('This report was sent in :```' + new Date() + '```');
 						break;
 					case config.prefix + 'help':
 						message.reply('Help goes here ...')
 						break;
 					default:
-						message.reply('**INVALID COMMAND')
+						message.reply('**INVALID COMMAND**')
 				}
 			}
 			catch(ex){console.log('The error is at switch statement : ' + ex.stack)}
 		}
 })
+
+exports.bot = bot
+exports.config = config
+
+newCommand = function (name, type, callback, aliases, description, usage) 
+{
+  exports.commands[type][name] = {}
+  exports.commands[type][name]['aliases'] = aliases
+  exports.commands[type][name]['description'] = description
+  exports.commands[type][name]['usage'] = usage
+  exports.commands[type][name]['process'] = callback
+}
+
+function LoadScript(path, reload) 
+{
+	var pathReq = require(path)
+
+	if (reload) 
+	{
+		console.log('Reloaded script @ ' + path)
+	} 
+	else
+	{
+		console.log('Loaded script @ ' + path)
+	}
+}
+
+
+exports.registerCommand = registerCommand
+exports.LoadScript = LoadScript
+
+//Loading all the scripts from ./scripts/ directory
+var scripts = fs.readdirSync('./scripts/')
+scripts.forEach(script => {
+  if (script.substring(script.length - 3, script.length) === '.js') 
+  {
+    exports.LoadScript('./script/' + script)
+  }
+})
+
 
 bot.login(process.env.BOT_TOKEN)
