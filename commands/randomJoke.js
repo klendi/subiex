35a899e3ce9d2efd95b42e49c8e26a168160d43a
Klendi Gocci <klendigocci@gmail.com>
2018-02-12 17:21:41 +0100

    :zap: Added Joke fix

--- commands/randomJoke.js
+++ commands/randomJoke.js
@@ -3,5 +3,8 @@ const Subiex = require('../bot.js');
 const oneLinerJoke = require('one-liner-joke');
 
 Subiex.registerCommand('joke', 'default', (message) => {
-  message.channel.send(oneLinerJoke.getRandomJoke());
+  console.log(oneLinerJoke.getRandomJoke());
+  var oneLinerJoke1 = oneLinerJoke.getRandomJoke();
+  console.log(oneLinerJoke1);
+  message.channel.send(oneLinerJoke1);
 }, ['emoji', 'funny', 'hilarious'], 'Get a random joke!', '[]')
