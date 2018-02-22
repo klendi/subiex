342688f6b8f00ee7b63419b522fbba5850f52d89
Klendi Gocci <klendigocci@gmail.com>
2018-02-22 16:17:42 +0100

    :zap: Working on remindme command

--- commands/remindme.js
+++ commands/remindme.js
@@ -3,13 +3,13 @@ const Subiex = require('../bot.js')
 const config = require('../config')
 
 Subiex.registerCommand('remindme', 'default', (message, bot) => {
-  var args = message.split(' ');
+  var args = message.content.split(' ');
   message.react("☑");
   message.channel.send('Done, creating a task with ', args[1] , ' Seconds');
 }, ['remind'], 'Creates a reminder. Pass without args to start a guided tour.', '[]')
 
 function createReminder(msg, timeInMinutes) {
   setTimeout(function() {
-    
+
   }, 10);
 }
