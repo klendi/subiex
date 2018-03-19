4cf4d1b3b2fdbee85be22e382873feb40f47a0ab
Klendi Gocci <klendigocci@gmail.com>
2018-03-19 19:25:35 +0100

    :zap: Add Spam Command Fix

--- commands/spam.js
+++ commands/spam.js
@@ -1,10 +1,18 @@
 const Discord = require('discord.js')
 const Subiex = require('../bot.js')
+const config = require('../config')
 
 Subiex.registerCommand('spam', 'default', (message, bot) => {
   let num = parseInt(message.content);
-
+  
+if(message.author.id == config.master)
+{
   for (var i = 0; i < num; i++) {
   	message.channel.send(":huehuehue:, " + message.author);
   }
+}
+else
+{
+	message.channel.send("You don't have permissions, only Klendi Have ")
+}
 }, ['spam'], 'Spam', '[]')
