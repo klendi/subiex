0adc6e1d22c40bfb11dd2a4e92696fcb9fd2da7d
klendi <klendigocci@gmail.com>
2018-06-14 15:56:09 +0200

    :zap: Change Status Message

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ bot.on('ready', () => {
     console.log('Subiex is online');
     bot.user.setActivity('World Cup 2018');
     // console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
-    bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
+    // bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
 
 
